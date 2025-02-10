2089017d3ad203d55c5ef04e3ab7358c8ec0f9b1
Vaibhav Kaushal <vaibhav-kaushal@users.noreply.github.com>
2025-02-10 10:13:59 +0530

    Update editinit.zsh
    update pr to clpr

--- editinit.zsh
+++ editinit.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
 if [[ ! -v ZSH_SESSION_NAME ]]; then
-  pr red "Not inside an active session."
+  clpr red "Not inside an active session."
   echo "Please join a session first."
   return 40
 fi
 
 if [ ! -d "${ZSHY_SESS_DATA_PATH}/active.${ZSH_SESSION_NAME}" ]; then
-  pr red "Active session's storage directory is not present!!!!!"
-  pr red "!!! ABORTING !!!"
+  clpr red "Active session's storage directory is not present!!!!!"
+  clpr red "!!! ABORTING !!!"
   return 39
 fi
 
