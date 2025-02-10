816079d984e65275f7be5108ec777b88b7386530
Vaibhav Kaushal <vaibhav-kaushal@users.noreply.github.com>
2025-02-10 10:13:12 +0530

    Update delete.zsh
    update pr to clpr

--- delete.zsh
+++ delete.zsh
@@ -1,28 +1,28 @@
 #!/usr/bin/env zsh
 
 if [ -d "${ZSHY_SESS_DATA_PATH}/active.${1}" ]; then
-  pr red "Cannot delete an active session!"
+  clpr red "Cannot delete an active session!"
   echo "Please end the session and try again."
   return 46
 fi
 
 if [ ! -d "${ZSHY_SESS_DATA_PATH}/ended.${1}" ]; then
-  pr red "No such session. Cannot delete a non-existent session!"
+  clpr red "No such session. Cannot delete a non-existent session!"
   return 47
 fi
 
-pr blue "Are you sure?"
-pr blue "-------------"
+clpr blue "Are you sure?"
+clpr blue "-------------"
 
 read -k1 "choice?Are you sure you want to delete session '${1}'? [y/n] "
 if [[ $choice = "y" || $choice = "Y" ]]; then
   echo ""
-  pr red "Deleting session"
+  clpr red "Deleting session"
   rm -r "${ZSHY_SESS_DATA_PATH}/ended.${1}"
-  pr green "...done"
+  clpr green "...done"
 else
-  pr blue "You opted for not deleting the session."
-  pr blue "!!! Aborting !!!"
+  clpr blue "You opted for not deleting the session."
+  clpr blue "!!! Aborting !!!"
   return 48
 fi
 
